a46b3096d871179aac2f2bd831e604e84db021a2
Roshan Shrestha <156121270+R0shanShrestha@users.noreply.github.com>
2025-06-22 02:21:43 +0545

    Update App.js

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/test", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to Server");
 });
 
