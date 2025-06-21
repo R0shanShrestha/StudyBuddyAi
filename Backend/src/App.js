64a2ebbac369475335e979cf0be2f27b8ab4aa26
Roshan Shrestha <156121270+R0shanShrestha@users.noreply.github.com>
2025-06-22 02:19:00 +0545

    correct backuri

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Welcome to Server");
 });
 
