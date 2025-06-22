ca725ca07abc5f1a139803dd1ff42fbe56bcfa07
Roshan Shrestha <156121270+R0shanShrestha@users.noreply.github.com>
2025-06-22 11:45:49 +0545

    correct backuri

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -10,7 +10,7 @@ const app = express();
 // Allow only your frontend origin
 app.use(
   cors({
-    origin: "https://study-buddy-ai-lac.vercel.app",
+    origin: ["https://study-buddy-ai-lac.vercel.app", "http://localhost:5173/"],
     credentials: true,
   })
 );
@@ -26,7 +26,6 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("WElcome to server");
 });
-
 
 app.use("/api/v1/auth/", AuthRoutes);
 app.use("/api/v1/ai/", AiRoutes);
